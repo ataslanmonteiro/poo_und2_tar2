a3e929a5937c54a10dfd57b21633dc53000526a5
Ataslan Monteiro <ataslan.monteiro@gmail.com>
2024-12-14 19:58:20 -0300

    correção 4 ok

--- 4.js
+++ 4.js
@@ -1,6 +1,6 @@
-const promt = require("prompt-sync")();
-let num1 = parseInt(promt("Digite o primeiro número: "));
-let num2 = parseInt(promt("Digite o segundo número: "));
+const prompt = require("prompt-sync")();
+let num1 = parseInt(prompt("Digite o primeiro número: "));
+let num2 = parseInt(prompt("Digite o segundo número: "));
 for (let i = num1; i <= num2; i++) {
   if (i % 2 !== 0) {
     console.log(i);
---
+++ 5.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")();
+let num = parseInt(promt("Digite um número: "));
+let primo = true;
+if (num <= 1) {
+  primo;
+}
+for (let i = num1; i <= num2; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}

e4cef9bd49b9945f5a5df74d62e6f9525a165fd6
Ataslan Monteiro <ataslan.monteiro@gmail.com>
2024-12-14 20:12:25 -0300

    teste 5 ok

--- 5.js
+++ 5.js
@@ -1,11 +1,20 @@
 const prompt = require("prompt-sync")();
-let num = parseInt(promt("Digite um número: "));
+let num = parseInt(prompt("Informe um número: "));
+
 let primo = true;
 if (num <= 1) {
-  primo;
-}
-for (let i = num1; i <= num2; i++) {
-  if (i % 2 !== 0) {
-    console.log(i);
+  primo = false;
+} else {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      primo = false;
+      break;
+    }
   }
+}
+
+if (primo) {
+  console.log(`O número ${num} é primo.`);
+} else {
+  console.log(`O número ${num} não é primo.`);
 }
